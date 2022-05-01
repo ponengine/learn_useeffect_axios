93e1276c9abee3816ec85a466489d12b16f55355
nathapon <pon_engine@hotmail.com>
2022-05-01 19:18:50 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect,useState } from 'react';
+import axios from 'axios';
+
 
 function App() {
+
+  const [data,setData] = useState([])
+useEffect(() => {
+  
+ axios.get('https://5f6ca2b134d1ef0016d585ae.mockapi.io/data').then(data=>data.data).then(data=>
+
+ setData(data));
+}, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
